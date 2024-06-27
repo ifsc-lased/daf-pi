3ca8e851450a8dd887c47ee0c20419c45d80db64
Mike <mikeicd@gmail.com>
2024-06-27 11:19:08 -0300

    Adicionados argumentos para indicar a porta serial e o timeout da camada arq.

--- app.py
+++ app.py
@@ -5,16 +5,27 @@ from daf_virtual_rasp.com.enq_enum import TIPO_t
 from daf_virtual_rasp.daf.daf import DAF
 import sys, time, os
 import serial
+import argparse
 
+
+# Definindo argumentos de linha de comando
+parser = argparse.ArgumentParser(description='DAF-pi')
+parser.add_argument('-p', '--port', type=str, default='/dev/ttyGS0', help='Porta serial de comunicação (default: /dev/ttyGS0)')
+parser.add_argument('-P', '--port_socat', type=int, help='Porta serial de comunicação usando socat (sobrescreve o argumento -p)')
+parser.add_argument('-t', '--timeout_arq', type=float, default=2, help='Tempo de timeout da camada de arq (default: 2 segundos)')
+args = parser.parse_args()
+
+# Recebendo argumentos de linha de comando
+port = f'/dev/pts/{args.port_socat}' if args.port_socat else args.port
+timeout_arq = args.timeout_arq
 
 # define parametros das camadas ARQ e Enquadramento
 max_tentativas_arq = 3
 timeout_enq = 0.5
-timeout_arq = 2
 
 # obtém porta serial 
 ser = serial.Serial(
-            port='/dev/ttyGS0',
+            port=port,
             baudrate = 115200,
             parity=serial.PARITY_NONE,
             stopbits=serial.STOPBITS_ONE,
